ca0a572efc8202c23d69f51480134c6bb5ade31b
Your Name <you@example.com>
2024-09-09 22:41:44 +0300

    first commit

---
+++ config/plugins.js
@@ -0,0 +1,21 @@
+// module.exports = ({ env }) => ({
+//     email: {
+//       config: {
+//         provider: 'nodemailer',
+//         providerOptions: {
+//           host: env('SMTP_HOST'),
+//           port: env('SMTP_PORT'),
+//           auth: {
+//             user: env('SMTP_USER'),
+//             pass: env('SMTP_PASS'),
+//           },
+//           secure: false, // تأكد من إعداد هذا بناءً على احتياجاتك
+//         },
+//         settings: {
+//           defaultFrom: 'noreply@example.com',
+//           defaultReplyTo: 'noreply@example.com',
+//         },
+//       },
+//     },
+//   });
+  
